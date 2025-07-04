9b3fe5a3992dc8e38752d1d94b8dfaa66892bba7
Andrew <a.c.g.hutani@student.tudelft.nl>
2025-07-04 11:46:35 +0200

    Attempt to visualize predictions

--- exps/run/RealtimePhysMop.py
+++ exps/run/RealtimePhysMop.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import torch
 import torch.nn as nn
 
@@ -258,3 +259,281 @@ if __name__ == "__main__":
 
         visualize_continuous_motion(pred_J_data.cpu().detach().numpy(), title='Predicted Joints Data')
         break
+=======
+import torch
+import torch.nn as nn
+
+import numpy as np 
+
+from models.PhysMoP import PhysMoP
+from models.humanmodel import SMPL, SMPLH
+
+import utils.config as config
+from utils.utils import remove_singlular_batch, smoothness_constraint, batch_roteulerSMPL, compute_errors, compute_error_accel_T
+
+import utils.constants as constants
+from dataclasses import dataclass
+
+from visualize_motion import visualize_continuous_motion, visualize_motion_with_ground_truth
+
+@dataclass
+class BatchInfo:
+    process_size_test: int
+    test_batchsize: int
+    gt_shape: torch.Tensor
+    gt_gender_id: torch.Tensor
+    gt_joints: torch.Tensor
+    gt_joints_smpl: torch.Tensor
+
+
+class RealtimePhysMop:
+    def __init__(self, checkpoint_path, device='auto'):
+        self.set_device(device)
+             
+        self.smpl = SMPL(device=self.device)
+        self.smplh_m = SMPLH(gender='male', device=self.device)
+        self.smplh_f = SMPLH(gender='female', device=self.device)
+
+        self.model = PhysMoP(hist_length=config.hist_length,
+                                       physics=True,
+                                       data=True,
+                                       fusion=True
+                                       ).to(self.device)
+        checkpoint = torch.load(checkpoint_path)
+        self.model.load_state_dict(checkpoint['model'], strict=True)
+        self.model.eval()
+
+        self.observed_motion = []
+        self.predicted_motion = []
+        self.ground_truth = [] 
+
+    def set_device(self, device):
+        # Device selection with availability checking
+        if device == 'auto':
+            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        elif device == 'cuda':
+            if not torch.cuda.is_available():
+                raise RuntimeError("CUDA device requested but CUDA is not available on this system")
+            self.device = torch.device('cuda')
+        elif device == 'mps':
+            if not torch.backends.mps.is_available():
+                raise RuntimeError("MPS device requested but MPS is not available on this system")
+            self.device = torch.device('mps')
+        elif device == 'cpu':
+            self.device = torch.device('cpu')
+        else:
+            raise ValueError(f"Invalid device '{device}'. Valid options are: 'auto', 'cuda', 'mps', 'cpu'")
+        
+        print(f"Using device: {self.device}")
+
+
+    def predict(self, data_test):
+        """
+        data_test: dict
+            Contains the following keys:
+            - 'q': Tensor of shape (Batch size, Frames = 50, 3) representing the joint angles
+            - 'shape': Tensor of shape (Batch size, Frames, 10) representing SMPL shape parameters (?)
+            - gender_id: Tensor of shape (Batch size, Frames) representing which gender to use
+        """
+        # Data Format Conversion & Extraction
+        gt_q = data_test['q'].type(torch.float32)
+        test_batchsize = gt_q.shape[0]
+        process_size_test = config.total_length * test_batchsize
+
+        # NxTx10 to N*Tx10
+        gt_shape = data_test['shape'].type(torch.float32).view(process_size_test, 10)
+        # NxTx1 to N*Tx1
+        gt_gender_id = data_test['gender_id'].type(torch.float32).view(process_size_test)
+
+        # Global Translation Normalization
+        gt_q[:,:,:3] = gt_q[:,:,:3] - gt_q[:,0:1,:3]
+
+        # Singular Batch Removal
+        gt_q = remove_singlular_batch(gt_q)
+        
+        # Velocity and Acceleration Computation
+        gt_q_dot, gt_q_ddot, _ = smoothness_constraint(gt_q, constants.dt)
+        gt_q_ddot = gt_q_ddot[:, 1:]  # Remove first frame (no acceleration defined)
+
+        #SMPL Pose Parameter Conversion + Forward Kinematics
+        gt_pose = torch.zeros([test_batchsize, config.total_length, 72]).type(torch.float32).to(self.device)
+        gt_pose[:, :, constants.G2Hpose_idx] = gt_q[:,:,3:].to(self.device)  # Map 60 dims to 72 SMPL dims
+        gt_pose = gt_pose.view(process_size_test, 72)
+        
+        # Send all tensors to device
+        gt_q = gt_q.to(self.device)
+        gt_q_ddot = gt_q_ddot.to(self.device)
+        gt_shape = gt_shape.to(self.device)
+        gt_gender_id = gt_gender_id.to(self.device)
+        gt_pose = gt_pose.to(self.device)
+
+        gt_vertices, gt_joints, gt_joints_smpl, gt_rotMat_individual = self.forward_kinematics(gt_pose, gt_shape, gt_gender_id, process_size_test, joints_smpl=True, vertices=True)
+        batch_info = BatchInfo(process_size_test, test_batchsize, gt_shape, gt_gender_id, gt_joints, gt_joints_smpl)
+        gt_vertices_norm, gt_M_inv, gt_JcT = None, None, None
+
+        # Predict future joint angle data
+        # In this function, the first hist_length frames are used as input, and the remaining frames are predicted
+        model_output = self.model.forward_dynamics(gt_vertices_norm, gt_q, gt_q_ddot, gt_M_inv, gt_JcT, self.device, mode='test')
+        return model_output, batch_info
+
+    
+    def forward_kinematics(self, pose, shape, gender_id, process_size_test, vertices=False, joints_smpl=False):
+        rotmat, rotMat_individual = batch_roteulerSMPL(pose)
+
+        output_smplh_m = self.smplh_m.forward(betas=shape[gender_id==0], rotmat=rotmat[gender_id==0])
+        output_smplh_f = self.smplh_f.forward(betas=shape[gender_id==1], rotmat=rotmat[gender_id==1])
+        output_smpl = self.smpl.forward(betas=shape[gender_id==2], rotmat=rotmat[gender_id==2])
+
+        if vertices:
+            vertices = torch.zeros([process_size_test, 6890, 3]).float().to(self.device)
+            vertices[gender_id==0] = output_smplh_m.vertices
+            vertices[gender_id==1] = output_smplh_f.vertices
+            vertices[gender_id==2] = output_smpl.vertices
+        else:
+            vertices = None
+
+        joints = torch.zeros([process_size_test, 17, 3]).float().to(self.device)
+        joints[gender_id==0] = output_smplh_m.joints[:, :17]
+        joints[gender_id==1] = output_smplh_f.joints[:, :17]
+        joints[gender_id==2] = output_smpl.joints[:, :17]
+
+        if joints_smpl==True:
+            joints_smpl = torch.zeros([process_size_test, 24, 3]).float().to(self.device)
+            joints_smpl[gender_id==0] = output_smplh_m.joints_smpl[:, :24]
+            joints_smpl[gender_id==1] = output_smplh_f.joints_smpl[:, :24]
+            joints_smpl[gender_id==2] = output_smpl.joints_smpl[:, :24]
+        else:
+            joints_smpl = None
+
+        return vertices, joints, joints_smpl, rotMat_individual
+
+    def model_output_to_3D_joints(self, model_output, batch_info, mode='train'):
+        # Unpack model output and batch info
+        pred_q_data, pred_q_physics_gt, pred_q_physics_pred, pred_q_fusion, pred_q_ddot_data, pred_q_ddot_physics_gt, fusion_weight = model_output
+        process_size_test = batch_info.process_size_test
+        test_batchsize = batch_info.test_batchsize
+        gt_shape = batch_info.gt_shape
+        gt_gender_id = batch_info.gt_gender_id
+        gt_joints = batch_info.gt_joints
+        gt_joints_smpl = batch_info.gt_joints_smpl
+
+        # data-driven
+        pred_pose_data = torch.zeros([test_batchsize,config.total_length,constants.n_smplpose]).type(torch.float32).to(self.device)
+
+        pred_pose_data[:,:,constants.G2Hpose_idx] = pred_q_data[:,:,3:]
+        pred_pose_data = pred_pose_data.reshape([process_size_test, 72])
+        _, pred_joints_data, pred_joints_smpl_data, _ = self.forward_kinematics(pred_pose_data, gt_shape, gt_gender_id, process_size_test, joints_smpl=True)
+
+        # physics-gt
+        pred_pose_physics_gt = torch.zeros([test_batchsize,config.total_length,constants.n_smplpose]).type(torch.float32).to(self.device)
+        if mode == 'train':
+            pred_pose_physics_gt[:,:,constants.G2Hpose_idx] = pred_q_physics_gt[:,:,3:]
+        else:
+            pred_pose_physics_gt[:,:,constants.G2Hpose_idx] = pred_q_physics_pred[:,:,3:]
+        pred_pose_physics_gt = pred_pose_physics_gt.reshape([process_size_test, 72])
+        _, pred_joints_physics_gt, pred_joints_smpl_physics_gt, _ = self.forward_kinematics(pred_pose_physics_gt, gt_shape, gt_gender_id, process_size_test, joints_smpl=True)
+
+        # fusion
+        pred_pose_fusion = torch.zeros([test_batchsize,config.total_length,constants.n_smplpose]).type(torch.float32).to(self.device)
+        pred_pose_fusion[:,:,constants.G2Hpose_idx] = pred_q_fusion[:,:,3:]
+        pred_pose_fusion = pred_pose_fusion.reshape([process_size_test, 72])
+        pred_vertices_fusion, pred_joints_fusion, pred_joints_smpl_fusion, _ = self.forward_kinematics(pred_pose_fusion, gt_shape, gt_gender_id, process_size_test, joints_smpl=True, vertices=True)
+
+        if config.test_mode == 'H36M':
+            gt_J = torch.cat([gt_joints, gt_joints[:, 8:9], gt_joints[:, 8:9],
+                            gt_joints[:, 13:14], gt_joints[:, 16:17],
+                            gt_joints_smpl[:, 22:23], gt_joints_smpl[:, 22:23], gt_joints_smpl[:, 22:23], 
+                            gt_joints_smpl[:, 23:], gt_joints_smpl[:, 23:], gt_joints_smpl[:, 23:],
+                            ], dim=1)
+            gt_J[:, [0,1,4]] = 0
+            pred_J_data = torch.cat([pred_joints_data, pred_joints_data[:, 8:9], pred_joints_data[:, 8:9],
+                            pred_joints_data[:, 13:14], pred_joints_data[:, 16:17],
+                            pred_joints_smpl_data[:, 22:23], pred_joints_smpl_data[:, 22:23], pred_joints_smpl_data[:, 22:23], 
+                            pred_joints_smpl_data[:, 23:], pred_joints_smpl_data[:, 23:], pred_joints_smpl_data[:, 23:],
+                            ], dim=1)
+            pred_J_data[:, [0,1,4]] = 0
+            pred_J_physics_gt = torch.cat([pred_joints_physics_gt, pred_joints_physics_gt[:, 8:9], pred_joints_physics_gt[:, 8:9],
+                                    pred_joints_physics_gt[:, 13:14], pred_joints_physics_gt[:, 16:17],
+                                    pred_joints_smpl_physics_gt[:, 22:23], pred_joints_smpl_physics_gt[:, 22:23], pred_joints_smpl_physics_gt[:, 22:23], 
+                                    pred_joints_smpl_physics_gt[:, 23:], pred_joints_smpl_physics_gt[:, 23:], pred_joints_smpl_physics_gt[:, 23:],
+                                    ], dim=1)
+            pred_J_physics_gt[:, [0,1,4]] = 0
+            pred_J_fusion = torch.cat([pred_joints_fusion, pred_joints_fusion[:, 8:9], pred_joints_fusion[:, 8:9],
+                                    pred_joints_fusion[:, 13:14], pred_joints_fusion[:, 16:17],
+                                    pred_joints_smpl_fusion[:, 22:23], pred_joints_smpl_fusion[:, 22:23], pred_joints_smpl_fusion[:, 22:23], 
+                                    pred_joints_smpl_fusion[:, 23:], pred_joints_smpl_fusion[:, 23:], pred_joints_smpl_fusion[:, 23:],
+                                    ], dim=1)
+            pred_J_fusion[:, [0,1,4]] = 0
+        else:
+            gt_J = gt_joints_smpl[:, 3:22]
+            pred_J_data = pred_joints_smpl_data[:, 3:22]
+            pred_J_physics_gt = pred_joints_smpl_physics_gt[:, 3:22]
+            pred_J_fusion = pred_joints_smpl_fusion[:, 3:22]
+
+        return gt_J, pred_J_data, pred_J_physics_gt, pred_J_fusion
+
+    def evaluation_metrics(self, gt_J, pred_J_data, pred_J_physics_gt, pred_J_fusion):
+        gt_J = gt_J.detach().cpu().numpy()
+        pred_J_data = pred_J_data.detach().cpu().numpy()
+        pred_J_physics_gt = pred_J_physics_gt.detach().cpu().numpy()
+        pred_J_fusion = pred_J_fusion.detach().cpu().numpy()
+
+        _, errors = compute_errors(gt_J.copy(), pred_J_data, 0)
+        error_test_data = np.array(errors).reshape([-1, config.total_length])
+
+        _, errors = compute_errors(gt_J.copy(), pred_J_physics_gt, 0)
+        error_test_physics_gt = np.array(errors).reshape([-1, config.total_length])
+
+        _, errors = compute_errors(gt_J.copy(), pred_J_fusion, 0)
+        error_test_fusion = np.array(errors).reshape([-1, config.total_length])
+
+        perjaccel = compute_error_accel_T(gt_J.copy(), pred_J_data, config.total_length, 0)
+        accel_data = (np.array(perjaccel) * constants.m2mm)
+
+        perjaccel = compute_error_accel_T(gt_J.copy(), pred_J_physics_gt, config.total_length, 0)
+        accel_physics_gt = (np.array(perjaccel) * constants.m2mm)
+        
+        perjaccel = compute_error_accel_T(gt_J.copy(), pred_J_fusion, config.total_length, 0)
+        accel_fusion = (np.array(perjaccel) * constants.m2mm)
+
+from torch.utils.data import DataLoader, Subset
+from dataset.base_dataset_test import BaseDataset_test
+import time
+
+def get_specific_batch(data_loader, batch_index):
+    """Get a specific batch by index without loading all batches into memory"""
+    for i, batch in enumerate(data_loader):
+        if i == batch_index:
+            return batch
+    return None
+
+ds = "H36M" 
+if __name__ == "__main__":
+    realtime_model = RealtimePhysMop('ckpt/PhysMoP/2025_07_01-20_18_08_2800.pt', device='auto')
+    dataset = BaseDataset_test(ds, config.DATASET_FOLDERS_TEST, config.hist_length)
+
+    data_loader = DataLoader(
+        dataset=dataset,
+        batch_size=1,
+        shuffle=False,
+        num_workers=8
+    )
+    print(f"Total batches in data_loader: {len(data_loader)}")
+
+    # Select specific batch efficiently
+    batch_index = 1294
+    specific_batch = get_specific_batch(data_loader, batch_index)
+
+    if specific_batch is not None:
+        print(f"Successfully retrieved batch {batch_index}")
+        print("Batch keys:", specific_batch.keys())
+        
+        model_output, batch_info = realtime_model.predict(specific_batch)
+        gt_J, pred_J_data, pred_J_physics_gt, pred_J_fusion = realtime_model.model_output_to_3D_joints(model_output, batch_info, mode='test')
+        print("Predicted joints shape:", pred_J_data.shape)
+        visualize_continuous_motion(gt_J.cpu().detach().numpy(), title="Predicted Joints Data")
+        
+        visualize_motion_with_ground_truth(pred_J_fusion.cpu().detach(), gt_J.cpu().detach())
+    else:
+        print(f"Batch {batch_index} not found")
+>>>>>>> Stashed changes
--- exps/run/visualize_motion.py
+++ exps/run/visualize_motion.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -98,4 +99,209 @@ def preprocess(filename):
         
 # filename = '{0}/{1}/{2}_{3}.txt'.format(config.h36m_anno_dir, "S1", "walking", 1)
 # motion_sequence = preprocess(filename)
+=======
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import os, sys
+
+from config import config
+from datasets.h36m_eval import H36MEval
+import torch
+from utils.misc import expmap2rotmat_torch, find_indices_256, find_indices_srnn, rotmat2xyz_torch
+
+def visualize_continuous_motion(motion_sequence, title="Continuous Motion Visualization"):
+    """
+    Visualize a continuous motion sequence in 3D.
+
+    :param motion_sequence: Numpy array of shape [num_frames, num_joints, 3] (motion sequence).
+    :param title: Title of the plot.
+    """
+    axes_limit = 2
+    # Define the connections between joints (skeleton structure)
+    connections = [
+        # Spine/Torso
+        (0, 7),   # Hip to Spine
+        (7, 8),   # Spine to Thorax  
+        (8, 9),   # Thorax to Neck/Nose
+        (9, 10),  # Neck to Head
+        
+        # Right leg
+        (0, 1),   # Hip to RHip
+        (1, 2),   # RHip to RKnee
+        (2, 3),   # RKnee to RFoot
+        
+        # Left leg  
+        (0, 4),   # Hip to LHip
+        (4, 5),   # LHip to LKnee
+        (5, 6),   # LKnee to LFoot
+        
+        # Right arm
+        (8, 14),  # Thorax to RShoulder
+        (14, 15), # RShoulder to RElbow
+        (15, 16), # RElbow to RWrist
+        
+        # Left arm
+        (8, 11),  # Thorax to LShoulder
+        (11, 12), # LShoulder to LElbow
+        (12, 13), # LElbow to LWrist
+    ]
+
+    # connections = [
+    #     (0 + 1, 1 + 1), (1 + 1, 2 + 1), (2 + 1, 3 + 1), (3 + 1, 4 + 1), (4 + 1, 5 + 1),
+    #     (0 + 1, 6 + 1), (6 + 1, 7 + 1), (7 + 1, 8 + 1), (8 + 1, 9 + 1), (9 + 1, 10 + 1),
+    #     (11 + 1, 12 + 1), (12 + 1, 13 + 1), (13 + 1, 14 + 1), (14 + 1, 15 + 1),
+    #     (16 + 1, 17 + 1), (17 + 1, 18 + 1), (18 + 1, 19 + 1), (19 + 1, 20 + 1), 
+    #     (20 + 1, 21 + 1), (21 + 1, 22 + 1), (22 + 1, 23 + 1),
+    #     (24 + 1, 25 + 1), (25 + 1, 26 + 1), (26 + 1, 27 + 1), (27 + 1, 28 + 1), 
+    #     (28 + 1, 29 + 1), (29 + 1, 30 + 1), (30 + 1, 31 + 1)
+    # ]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for frame_idx in range(motion_sequence.shape[0]):
+        ax.clear()
+        ax.set_xlim([-1, 1])
+        ax.set_ylim([-1, 1])
+        ax.set_zlim([-1, 1])
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+
+        ax.set_title(f"Frame {frame_idx}: {motion_sequence[frame_idx, 0]}")
+
+        joints = motion_sequence[frame_idx]
+        ax.scatter(joints[:, 0], joints[:, 2], joints[:, 1], c='r', marker='o')
+
+         # Add joint indices as text annotations
+        for joint_idx, (x, z, y) in enumerate(joints):
+            ax.text(x, y, z, str(joint_idx), color='blue', fontsize=8)
+        for connection in connections:
+            joint1, joint2 = connection
+            ax.plot([joints[joint1, 0], joints[joint2, 0]],
+                    [joints[joint1, 2], joints[joint2, 2]],
+                    [joints[joint1, 1], joints[joint2, 1]], 'r', alpha=0.5)
+
+        plt.pause(0.5)  # Adjust the pause duration for smoother animation
+
+    plt.show()
+
+def preprocess(filename):
+    info = open(filename, 'r').readlines()
+    pose_info = []
+    for line in info:
+        line = line.strip().split(',')
+        if len(line) > 0:
+            pose_info.append(np.array([float(x) for x in line]))
+    pose_info = np.array(pose_info)
+    pose_info = pose_info.reshape(-1, 33, 3)  # [num_frames, num_joints, 3]
+    
+    # Convert exponential maps to rotation matrices
+    pose_info_flat = pose_info.reshape(-1, 3)  # Flatten for conversion
+    rotation_matrices = expmap2rotmat_torch(torch.tensor(pose_info_flat).float())
+    
+    rotation_matrices = rotation_matrices.reshape(-1, 33, 3, 3)
+    root_rotation = rotation_matrices[:, 0, :, :].clone().detach()
+    print("Root rotation shape: ", root_rotation.shape)
+
+    joint_positions = rotmat2xyz_torch(rotation_matrices[:, 1:, :, :])  # Convert to XYZ format
+    print("Joint positions shape: ", joint_positions.shape)
+
+    root_translation = pose_info[:, 0, :]
+
+
+    rotated_joint_positions = torch.matmul(root_rotation.unsqueeze(1), joint_positions.unsqueeze(-1))
+    rotated_joint_positions = rotated_joint_positions.squeeze(-1)  # Remove the last dimension
+    print("Root position unsqueeze shape: ", torch.tensor(root_translation).float().unsqueeze(1).shape)
+    rotated_joint_positions += torch.tensor(root_translation).float().unsqueeze(1)
+
+    return rotated_joint_positions
+
+def visualize_motion_with_ground_truth(predicted_positions, ground_truth_positions, title="Predicted vs Ground Truth Motion"):
+    """
+    Visualize the predicted motion and ground truth in 3D for specific time steps, with skeleton connections.
+
+    :param predicted_positions: Tensor of shape [num_frames, num_joints, 3] (predicted motion)
+    :param ground_truth_positions: Tensor of shape [num_frames, num_joints, 3] (ground truth motion)
+    :param time_steps: List of time steps to visualize (e.g., [2, 10, 14, 25])
+    :param title: Title of the plot
+    """
+    # Define the connections between joints
+    connections = [
+        # Spine/Torso
+        (0, 7),   # Hip to Spine
+        (7, 8),   # Spine to Thorax  
+        (8, 9),   # Thorax to Neck/Nose
+        (9, 10),  # Neck to Head
+        
+        # Right leg
+        (0, 1),   # Hip to RHip
+        (1, 2),   # RHip to RKnee
+        (2, 3),   # RKnee to RFoot
+        
+        # Left leg  
+        (0, 4),   # Hip to LHip
+        (4, 5),   # LHip to LKnee
+        (5, 6),   # LKnee to LFoot
+        
+        # Right arm
+        (8, 14),  # Thorax to RShoulder
+        (14, 15), # RShoulder to RElbow
+        (15, 16), # RElbow to RWrist
+        
+        # Left arm
+        (8, 11),  # Thorax to LShoulder
+        (11, 12), # LShoulder to LElbow
+        (12, 13), # LElbow to LWrist
+    ]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Set the default viewing angle
+    ax.view_init(elev=20, azim=55)  # Adjust elevation and azimuth as needed
+    ax.set_title(title)
+
+    for frame_idx in range(len(predicted_positions)):
+        ax.clear()
+        ax.set_xlim([-1, 1])
+        ax.set_ylim([-1, 1])
+        ax.set_zlim([-1, 1])
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+
+        # Update the title to include the current time step
+        ax.set_title(f"{title} - Time Step #{frame_idx}")
+
+        # Plot predicted joints for the specific frame
+        predicted_joints = predicted_positions[frame_idx - 1]  # Subtract 1 because time_steps are 1-based
+        ax.scatter(predicted_joints[:, 0], predicted_joints[:, 2], predicted_joints[:, 1], c='r', marker='o', label='Predicted')
+
+        # Plot ground truth joints for the specific frame
+        ground_truth_joints = ground_truth_positions[frame_idx - 1]
+        ax.scatter(ground_truth_joints[:, 0], ground_truth_joints[:, 2], ground_truth_joints[:, 1], c='b', marker='^', label='Ground Truth')
+
+        ax.legend()
+        # Draw skeleton connections for predicted motion
+        for connection in connections:
+            joint1, joint2 = connection
+            ax.plot([predicted_joints[joint1, 0], predicted_joints[joint2, 0]],
+                    [predicted_joints[joint1, 2], predicted_joints[joint2, 2]],
+                    [predicted_joints[joint1, 1], predicted_joints[joint2, 1]], 'r', alpha=0.5)
+            ax.plot(
+                [ground_truth_joints[joint1, 0], ground_truth_joints[joint2, 0]],
+                [ground_truth_joints[joint1, 2], ground_truth_joints[joint2, 2]],
+                [ground_truth_joints[joint1, 1], ground_truth_joints[joint2, 1]],
+                c='b'
+            )  
+
+        plt.pause(0.5)  # Pause to display each frame
+
+    plt.show()
+        
+# filename = '{0}/{1}/{2}_{3}.txt'.format(config.h36m_anno_dir, "S1", "walking", 1)
+# motion_sequence = preprocess(filename)
+>>>>>>> Stashed changes
 # visualize_continuous_motion(motion_sequence/1000., title="Continuous Motion Visualization")
